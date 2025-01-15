a11943f094e66ad1a918720e449a00f699c75002
cost12 <51209490+cost12@users.noreply.github.com>
2025-01-14 21:29:50 -0500

    improvements to print visualizer

--- small_version/print_visualizer.py
+++ small_version/print_visualizer.py
@@ -4,7 +4,10 @@ from pokemon_card import PokemonCard, Attack, stage_to_str
 from pokemon_battle import ActivePokemon, OwnDeckView, OpponentDeckView
 
 def visualize_attack(attack:Attack, indent:str="") -> None:
-    print(f"{indent}{attack.name} {attack.base_damage}\n{attack.text}\nCost: {attack.energy_cost}")
+    if attack.text == "":
+        print(f"{indent}{attack.name} {attack.base_damage}\nCost: {attack.energy_cost}")
+    else:
+        print(f"{indent}{attack.name} {attack.base_damage}\n{attack.text}\nCost: {attack.energy_cost}")
 
 def visualize_active_pokemon(pokemon:ActivePokemon, indent:str="") -> None:
     print(f"{indent}{pokemon.active_card().card_type} {stage_to_str(pokemon.active_card().pokemon.get_stage())} {pokemon.active_card().name()} {pokemon.hp()} HP")
@@ -15,11 +18,12 @@ def visualize_active_pokemon(pokemon:ActivePokemon, indent:str="") -> None:
         visualize_attack(attack,f"{indent}[{i}]")
         i+=1
     print(f"{indent}Retreat: {pokemon.active_card().retreat_cost}")
+    print(f"{indent}Energy: {pokemon.energies}")
 
 def visualize_card(card:PokemonCard, indent:str="") -> None:
     print(f"{indent}{card.card_type.name} {stage_to_str(card.pokemon.get_stage())} {card.name()} {card.hit_points} HP")
     if not card.is_basic():
-        print(f"{indent}Evolves from {card.evolves_from().name()}")
+        print(f"{indent}Evolves from {card.evolves_from().name}")
     i=0
     for attack in card.attacks:
         visualize_attack(attack, f"{indent}[{i}]")
@@ -27,7 +31,7 @@ def visualize_card(card:PokemonCard, indent:str="") -> None:
     print(f"{indent}Retreat: {card.retreat_cost}")
 
 def visualize_active_pokemon_quick(pokemon:ActivePokemon, indent:str="") -> None:
-    print(f"{indent}{pokemon.active_card().card_type} {stage_to_str(pokemon.active_card().pokemon.get_stage())} {pokemon.active_card().name()} {pokemon.hp()} HP")
+    print(f"{indent}{pokemon.active_card().card_type.name} {stage_to_str(pokemon.active_card().pokemon.get_stage())} {pokemon.active_card().name()} {pokemon.hp()} HP")
 
 def visualize_card_quick(card:PokemonCard, indent:str="") -> None:
     print(f"{indent}{card.card_type.name} {stage_to_str(card.pokemon.get_stage())} {card.name()} {card.hit_points} HP")
@@ -35,11 +39,11 @@ def visualize_card_quick(card:PokemonCard, indent:str="") -> None:
 def visualize_actives(active:tuple[ActivePokemon], indent:str="") -> None:
     print(f"{indent}Active:")
     if len(active) > 0:
-        visualize_active_pokemon_quick(active[0], f"{indent}[0]")
+        visualize_active_pokemon_quick(active[0], f"{indent}\t[0] ")
         print(f"{indent}Bench:")
         i=1
         for card in active[1:]:
-            visualize_active_pokemon_quick(card, f"{indent}[{i}]")
+            visualize_active_pokemon_quick(card, f"{indent}\t[{i}] ")
             i+=1
     else:
         print(f"{indent}Empty")
@@ -48,7 +52,7 @@ def visualize_card_list(cards:tuple[PokemonCard], type:str, indent:str="") -> No
     print(f"{indent}{type}:")
     i=0
     for card in cards:
-        visualize_card_quick(card, f"{indent}[{i}]")
+        visualize_card_quick(card, f"{indent}\t[{i}] ")
         i+=1
 
 def visualize_own_deck(deck:OwnDeckView, indent:str="") -> None:
